6aa663c92eefa7c60129335912a3a7eceadcc229
andersonkaiti <anderkaiti@gmail.com>
2024-11-24 21:14:39 -0300

    :lipstick: feat: Alterando o tamanho dos textos da tela do Sindicato e adicionando a vigência da diretoria.

--- resources/views/syndicate/syndicate.blade.php
+++ resources/views/syndicate/syndicate.blade.php
@@ -11,7 +11,7 @@
         </div>
     </div>
     <div class="max-w-7xl mx-auto w-full flex flex-col gap-8 p-7 md:p-20">
-        <h1 class="text-[#138942] font-sans font-bold text-3xl leading-[40px] self-stretch">
+        <h1 class="text-[#138942] font-sans font-bold text-3xl">
             Sindicato
         </h1>
 
@@ -19,14 +19,14 @@
             <h3 class="text-[#454545] font-bold text-2xl">
                 O que é o Sindicato?
             </h3>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Pela trajetória de luta do sindicalismo no Brasil, não é difícil compreender o que é o Sindicato.
             </p>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 O Sindicato foi um espaço construído pelos próprios trabalhadores no desenvolvimento da história, apesar da sua oficialização enquanto instituição pelo Estado.
                 A simples existência de uma sede com seu corpo administrativo é insuficiente para conceituar o Sindicato. Ele é muito mais do que isso.
             </p>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Sociologicamente, podemos afirmar que o Sindicato é o resultado da ação das massas organizadas em busca de conquistas imediatas e profundas, como salário, emprego, participação política, etc.
             </p>
         </div>
@@ -35,17 +35,17 @@
             <h3 class="text-[#454545] font-bold text-2xl">
                 Para que serve o Sindicato?
             </h3>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Em síntese, o Sindicato serve para defender os interesses da categoria e organizá-la para conquistas coletivas em defesa de seus direitos e necessidades.
             </p>
             <br>
         </div>
 
         <div class="flex flex-col gap-4">
-            <h1 class="text-[#138942] font-sans font-bold text-2xl self-stretch">
+            <h1 class="text-[#138942] font-sans font-bold text-2xl">
                 Como funciona o nosso sindicato?
             </h1>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Nosso Sindicato funciona com uma diretoria eletiva a cada cinco anos, escolhida a partir de um programa de trabalho.
                 Essa diretoria se reúne para discutir o dia a dia do Sindicato, traçar e propor planos de trabalho, de luta e seus encaminhamentos.
             </p>
@@ -55,7 +55,7 @@
             <h3 class="text-[#454545] font-bold text-2xl">
                 Frentes de trabalho
             </h3>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Nosso Sindicato se divide em frentes de trabalho, tendo como finalidade uma melhor organização e maior agilidade nos encaminhamentos; Imprensa e Comunicação; Assistência, Saúde, Lazer e Cultura; Jurídico; Formação Sindical; Previdência; Assuntos Intersindicais e Assuntos Parlamentares.
             </p>
         </div>
@@ -64,7 +64,7 @@
             <h3 class="text-[#454545] font-bold text-2xl">
                 Como são tomadas as decisões no SINPRO - Instâncias de deliberação
             </h3>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Todos os associados participam das assembleias, que são a instância de decisão abaixo do Congresso que acontece a cada 3 anos. Outro nível de decisão é a Diretoria efetiva, que dirige o SINPRO num período de cinco anos. Todo sindicalizado pode concorrer à eleição. Outra forma de participação é como delegado sindical.
             </p>
         </div>
@@ -73,22 +73,30 @@
             <h3 class="text-[#454545] font-bold text-2xl">
                 Abrangência
             </h3>
-            <p class="text-[#454545]">
+            <p class="text-[#454545] text-xl text-justify">
                 Bauru, Águas de Santa Bárbara, Agudos, Arandu, Arealva, Areiópolis, Avaí, Avaré, Bariri, Barra Bonita, Bocaina, Boracéia, Borebi, Botucatu, Cabrália Paulista, Cerqueira César, Dois Córregos, Duartina, Espírito Santo do Turvo, Fartura, Igaraçu do Tietê, Ipaussu, Itapuí, Itatinga, Lençóis Paulista, Macatuba, Manduri, Mineiros do Tietê, Óleo, Pardinho, Paulistânia, Pederneiras, Piraju, Pirajuí, Piratininga, Pratânia, Presidente Alves, São Manuel, São Pedro do Turvo, Sarutaiá, Taguaí, Tejupá e Timburi.
             </p>
         </div>
 
-        <h1 class="text-[#138942] font-bold text-2xl self-stretch">
+        <h1 class="text-[#138942] font-bold text-2xl">
             Diretoria
         </h1>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 md2:grid-cols-4 w-full gap-8 justify-items-center">
-            @foreach ($directors as $director)
-                <x-director
-                    :role="$director->role"
-                    :name="$director->name"
-                />
-            @endforeach
+        <div class="flex flex-col gap-2">
+            <h1 class="text-[#454545] text-center text-3xl font-bold">
+                Conheça a nossa Diretoria
+            </h1>
+            <h2 class="text-[#454545] text-center text-2xl mb-4">
+                do SINPROBAU com mandato vigente {{ date("Y", strtotime($direction->start_date)) }}-{{ date("Y", strtotime($direction->end_date)) }}
+            </h2>
+            <div class="grid grid-cols-1 md:grid-cols-2 md2:grid-cols-4 w-full gap-8 justify-items-center">
+                @foreach ($directors as $director)
+                    <x-director
+                        :role="$director->role"
+                        :name="$director->name"
+                    />
+                @endforeach
+            </div>
         </div>
     </div>
 @endsection
