43026a7b4cef4f01a01e9c3962ec2513fb7c0b29
Alex Maximiano <avm3.gama@gmail.com>
2025-03-31 09:27:50 -0300

    Ajuste Layout: Componentização do Footer | Data Automática | Convênios

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -52,6 +52,9 @@
                         <a href="/carteirinha-virtual" class="hover:text-[#138942] font-bold outline-none">Carteirinha Virtual</a>
                     </li>
                     <li>
+                        <a href="#" class="hover:text-[#138942] font-bold outline-none">Convênios</a>
+                    </li>
+                    <li>
                         <a href="/noticias" class="hover:text-[#138942] font-bold outline-none">Notícias</a>
                     </li>
                     <li>
@@ -66,63 +69,7 @@
         <main>
             @yield("content")
         </main>
-        <footer class="w-full bg-[#1B5E1F]">
-            <div class="grid gap-8 grid-cols-1 md:grid-cols-2 md2:grid-cols-4 p-10 sm:px-32 sm:py-16">
-                <div class="flex flex-col items-start gap-3">
-                    <img src="/images/logo-sinprobau-footer.webp" alt="Logo SINPROBAU" class="w-40">
-                    <div class="flex gap-5">
-                        <a href="https://www.instagram.com/sinprobau?igsh=dDV3eTN3Y2htbXd0" target="_blank">
-                            <img src="/images/icons/mdi_instagram.svg" alt="Instagram" class="w-6 h-6">
-                        </a>
-                        <a href="https://www.facebook.com/sinprobauru?mibextid=ZbWKwL" target="_blank">
-                            <img src="/images/icons/ic_baseline-facebook.svg" alt="Facebook" class="w-6 h-6">
-                        </a>
-                        <a href="https://www.youtube.com/channel/UCrH5GgS-k0VkvI2xiUqppbA" target="_blank">
-                            <img src="/images/icons/mdi_youtube.svg" alt="YouTube" class="w-6 h-6">
-                        </a>
-                        <a href="https://api.whatsapp.com/send?phone=551438795313" target="_blank">
-                            <img src="/images/icons/ic_baseline-whatsapp.svg" alt="WhatsApp" class="w-6 h-6">
-                        </a>
-                    </div>
-                    <a href="/sistema" class="text-white hover:text-black border border-white hover:bg-white focus:ring-4 focus:outline-none focus:ring-[#A5D6A7] font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
-                        Logar como Admin
-                    </a>
-                </div>
-
-                <div class="text-white flex flex-col gap-3">
-                    <a href="/" class="w-fit hover:underline">Home</a>
-                    <a href="/seja-socio" class="w-fit hover:underline">Seja sócio</a>
-                    <a href="/contato" class="w-fit hover:underline">Contato</a>
-                    <a href="/noticias" class="w-fit hover:underline">Notícia</a>
-                    <a href="/carteirinha-virtual" class="w-fit hover:underline">Carteirinha Virtual</a>
-                </div>
-
-                <div class="text-white flex flex-col gap-3">
-                    <a href="/convencoes/sesi-senai" class="w-fit hover:underline">Convenção Sesi/Senai</a>
-                    <a href="/convencoes/ensino-superior" class="w-fit hover:underline">Convenção Ensino Superior</a>
-                    <a href="/convencoes/educacao-basica" class="w-fit hover:underline">Convenção Educação Básica</a>
-                    <a href="/convencoes/senac" class="w-fit hover:underline">Convenção Senac</a>
-                </div>
-
-                <div class="text-white flex flex-col gap-3">
-                    <p>SINDICATO DOS PROFESSORES DE BAURU</p>
-                    <p>Rua Capitão Gomes Duarte,</p>
-                    <p>6-74 - Bauru - SP</p>
-                    <p>Fone-Fax: (14) 3879-5313</p>
-                    <div class="flex gap-2">
-                        <a href="https://contee.org.br/contee/" target="_blank"><img src="/images/icons/contee.webp" alt="Logo CONTEE" class="h-12 object-contain"></a>
-                        <a href="https://www.ctb.org.br/" target="_blank"><img src="/images/icons/ctb.webp" alt="Logo CTB" class="h-12 object-contain"></a>
-                        <a href="https://fepesp.org.br" target="_blank"><img src="/images/icons/fepesp.webp" alt="Logo FEPESP" class="h-12 object-contain"></a>
-                    </div>
-                </div>
-
-            </div>
-            <div class="bg-[#1A2C17] p-2 text-center">
-                <p class="text-white text-sm">
-                    Sindicato dos Professores de Bauru 2024 - Todos os direitos reservados
-                </p>
-            </div>
-        </footer>
+        @include('components.footer')
         @vite(["resources/js/navigation-bar.js", "resources/js/multi-step-form.js", "resources/js/become-a-member.js"])
     </body>
 </html>
